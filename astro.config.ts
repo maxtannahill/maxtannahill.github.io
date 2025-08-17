d85baa1f25088573dc7f797702e9446cf34000f1
Max Tannahill <max@myemail.io>
2025-08-17 23:51:35 +0800

    Changes to config

--- astro.config.ts
+++ astro.config.ts
@@ -116,7 +116,7 @@ export default defineConfig({
   // https://docs.astro.build/en/guides/prefetch/
   prefetch: true,
   // ! Please remember to replace the following site property with your own domain
-  site: "http://astrocitrus.artemkutsan.pp.ua/",
+  site: 'https://maxtannahill.github.io/',
   vite: {
     build: {
       sourcemap: true, // Source maps generation
--- src/content/series/citrus-docs.md
+++ src/content/series/citrus-docs.md
@@ -1,6 +1,6 @@
 ---
 id: citrus-docs
-title: "Citrus Docs"
+title: "Citrus Doc"
 description: "Astro Citrus documentation outlines key aspects of the template, describing its core functionality for blog management and project documentation setup"
 featured: true
 ---
